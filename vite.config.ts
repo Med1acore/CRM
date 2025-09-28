457f372c774663dc9025db54f51c5151db10a2dc
Craxpaxfacts <fh.mediacore@gmail.com>
2025-09-28 19:37:14 +0300

    fix viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crm/',
+  // ИЗМЕНЕНИЕ БЫЛО ЗДЕСЬ: Указываем правильное имя репозитория
+  base: '/CRMCHURCH/',
   server: {
     port: 5177,
     open: true,
